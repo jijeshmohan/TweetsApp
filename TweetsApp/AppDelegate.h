01bc9b15dd70a2b355213b14a0a528d368a3edee
Jijesh Mohan <jijeshmohan@gmail.com>
2013-04-19 21:25:35 +0530

    Initial Commit

---
+++ TweetsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TweetsApp
+//
+//  Created by Jijeshm on 19/04/13.
+//  Copyright (c) 2013 Jijesh Mohan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweetsApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TweetsApp
+//
+//  Created by Jijeshm on 19/04/13.
+//  Copyright (c) 2013 Jijesh Mohan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
